9619d9672a2378f6475edce964aea429b5c17430
Luiz Gustavo <gusstavopdm@gmail.com>
2023-04-19 11:39:05 -0300

    🎉 feat(in-memory-users-repository.ts): add InMemoryUsersRepository class with findByEmail and create methods

---
+++ src/repositories/users/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,32 @@
+import { Prisma, User } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+
+import { UsersRepository } from '../users-repository'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public items: User[] = []
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = this.items.find((user) => user.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    const user = {
+      id: randomUUID(),
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    }
+
+    this.items.push(user)
+
+    return user
+  }
+}
