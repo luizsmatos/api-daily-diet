3027944cd7a0999d75f2139771b25643bf4da739
Luiz Gustavo <gusstavopdm@gmail.com>
2023-04-20 12:32:47 -0300

    🚀 feat(meals-repository.ts): add findById method to MealsRepository interface

--- src/repositories/meals/meals-repository.ts
+++ src/repositories/meals/meals-repository.ts
@@ -1,5 +1,6 @@
 import { Meal, Prisma } from '@prisma/client'
 
 export interface MealsRepository {
+  findById(mealId: string): Promise<Meal | null>
   create(data: Prisma.MealUncheckedCreateInput): Promise<Meal>
 }
