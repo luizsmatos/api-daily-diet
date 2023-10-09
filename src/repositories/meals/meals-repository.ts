74f21d0d42e48a90939490c5ecd78dd2f2e18a06
Luiz Gustavo <gusstavopdm@gmail.com>
2023-10-09 17:50:09 -0300

    🚀 feat(meals-repository.ts): add findManyByUserId method to MealsRepository interface to retrieve multiple meals by user ID

--- src/repositories/meals/meals-repository.ts
+++ src/repositories/meals/meals-repository.ts
@@ -1,6 +1,7 @@
 import { Meal, Prisma } from '@prisma/client'
 
 export interface MealsRepository {
+  findManyByUserId(id: string): Promise<Meal[]>
   findById(mealId: string): Promise<Meal | null>
   create(data: Prisma.MealUncheckedCreateInput): Promise<Meal>
   save(data: Meal): Promise<Meal>
