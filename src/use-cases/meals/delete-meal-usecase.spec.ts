b93233e41d574a6dc0014adf3e55748868814cef
Luiz Gustavo <gusstavopdm@gmail.com>
2023-10-09 16:45:17 -0300

    🔨 chore: add check to throw NotAllowedError if the user is not allowed to delete the meal

--- src/use-cases/meals/delete-meal-usecase.spec.ts
+++ src/use-cases/meals/delete-meal-usecase.spec.ts
@@ -2,8 +2,10 @@ import { beforeEach, describe, expect, it } from 'vitest'
 
 import { InMemoryMealsRepository } from '@/repositories/meals/in-memory/in-memory-meals-repository'
 
+import { makeMeal } from 'test/factories/make-meal'
 import { DeleteMealUseCase } from './delete-meal-usecase'
 import { MealNotFoundError } from './errors/meal-not-found-error'
+import { NotAllowedError } from './errors/not-allowed-error'
 
 let mealsRepository: InMemoryMealsRepository
 let sut: DeleteMealUseCase
@@ -15,16 +17,10 @@ describe('Delete Meal UseCase', () => {
   })
 
   it('should be able to delete a meal', async () => {
-    const createdMeal = await mealsRepository.create({
-      user_id: 'user-01',
-      name: 'JavaScript Meal',
-      description: 'A simple JavaScript meal',
-      date: '2020-01-01',
-      time: '12:00',
-      is_in_diet: true,
-    })
+    const newMeal = makeMeal()
+    mealsRepository.items.push(newMeal)
 
-    await sut.execute({ mealId: createdMeal.id })
+    await sut.execute({ userId: newMeal.user_id, mealId: newMeal.id })
 
     expect(mealsRepository.items).toHaveLength(0)
   })
@@ -32,8 +28,21 @@ describe('Delete Meal UseCase', () => {
   it("should not be able to delete a meal if it doesn't exist", async () => {
     await expect(
       sut.execute({
+        userId: 'user-1',
         mealId: 'meal-not-exists',
       }),
     ).rejects.toBeInstanceOf(MealNotFoundError)
+  })
+
+  it('should not be able to delete a meal from another user', async () => {
+    const newMeal = makeMeal()
+    mealsRepository.items.push(newMeal)
+
+    await expect(
+      sut.execute({
+        userId: 'another-user',
+        mealId: newMeal.id,
+      }),
+    ).rejects.toBeInstanceOf(NotAllowedError)
   })
 })
--- src/use-cases/meals/delete-meal-usecase.ts
+++ src/use-cases/meals/delete-meal-usecase.ts
@@ -1,19 +1,25 @@
 import { MealsRepository } from '@/repositories/meals/meals-repository'
 
 import { MealNotFoundError } from './errors/meal-not-found-error'
+import { NotAllowedError } from './errors/not-allowed-error'
 
 interface DeleteMealUseCaseRequest {
+  userId: string
   mealId: string
 }
 
 export class DeleteMealUseCase {
   constructor(private readonly mealsRepository: MealsRepository) {}
 
-  async execute({ mealId }: DeleteMealUseCaseRequest): Promise<void> {
+  async execute({ userId, mealId }: DeleteMealUseCaseRequest): Promise<void> {
     const meal = await this.mealsRepository.findById(mealId)
 
     if (!meal) {
       throw new MealNotFoundError()
+    }
+
+    if (meal.user_id !== userId) {
+      throw new NotAllowedError()
     }
 
     await this.mealsRepository.delete(meal.id)
