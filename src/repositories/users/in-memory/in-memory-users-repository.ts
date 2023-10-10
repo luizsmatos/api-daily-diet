ce9f5267e93b5d2af8884062e60e2c3689ef6e8e
Luiz Gustavo <gusstavopdm@gmail.com>
2023-10-10 12:34:27 -0300

    🔍 chore(in-memory-users-repository.ts): add findById method to InMemoryUsersRepository to retrieve a user by their id

--- src/repositories/users/in-memory/in-memory-users-repository.ts
+++ src/repositories/users/in-memory/in-memory-users-repository.ts
@@ -6,6 +6,16 @@ import { UsersRepository } from '../users-repository'
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
 
+  async findById(id: string): Promise<User | null> {
+    const user = this.items.find((user) => user.id === id)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
   async findByEmail(email: string): Promise<User | null> {
     const user = this.items.find((user) => user.email === email)
 
