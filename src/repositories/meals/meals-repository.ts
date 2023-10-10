2f5361e2113e632594f7c3456efeabd3c0540bbc
Luiz Gustavo <gusstavopdm@gmail.com>
2023-10-10 12:44:19 -0300

    🔧 refactor(meals-repository.ts): change return type of create and save methods to Promise<void> for consistency and clarity

--- src/repositories/meals/meals-repository.ts
+++ src/repositories/meals/meals-repository.ts
@@ -3,7 +3,7 @@ import { Meal, Prisma } from '@prisma/client'
 export interface MealsRepository {
   findManyByUserId(id: string): Promise<Meal[]>
   findById(mealId: string): Promise<Meal | null>
-  create(data: Prisma.MealUncheckedCreateInput): Promise<Meal>
-  save(data: Meal): Promise<Meal>
+  create(data: Prisma.MealUncheckedCreateInput): Promise<void>
+  save(data: Meal): Promise<void>
   delete(mealId: string): Promise<void>
 }
