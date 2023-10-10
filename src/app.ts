f4fd9e6eb9b35280dd5e2d5a5dd74d2e34b5c449
Luiz Gustavo <gusstavopdm@gmail.com>
2023-10-10 12:30:44 -0300

    🔧 chore(app.ts): add error handling

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
 import fastify from 'fastify'
+import { ZodError } from 'zod'
 
 import { usersRoutes } from './http/controllers/users/routes'
+import { env } from './env'
 
 const app = fastify()
 
 app.register(usersRoutes)
+
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
 
 export { app }
