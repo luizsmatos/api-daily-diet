11bdf63b0f3903972dadfdb8b2d73411c66e1a25
Luiz Gustavo <gusstavopdm@gmail.com>
2023-10-10 12:54:59 -0300

    ✨ feat(routes.ts): add route for user authentication at /sessions to enable user login functionality

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify'
 
 import { registerUserController } from './register-user-controller'
+import { authenticateUserController } from './authenticate-user-controller'
 
 export async function usersRoutes(app: FastifyInstance): Promise<void> {
   app.post('/users', registerUserController)
+  app.post('/sessions', authenticateUserController)
 }
