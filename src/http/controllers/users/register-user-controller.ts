50f8ec552ec64f2b1b800273bdb1272165e7321b
Luiz Gustavo <gusstavopdm@gmail.com>
2023-10-10 12:17:43 -0300

    ✨ feat(register-user-controller.ts): add registerUserController function to handle user registration requests
    This commit adds a new file `register-user-controller.ts` which contains the `registerUserController` function. This function handles user registration requests by validating the request body using the `registerBodySchema` schema and calling the `registerUserUseCase` to register the user. If the user already exists, a `UserAlreadyExistsError` is thrown and a 409 status code is returned. Otherwise, a 201 status code is returned.

---
+++ src/http/controllers/users/register-user-controller.ts
@@ -0,0 +1,31 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { UserAlreadyExistsError } from '@/use-cases/users/errors/user-already-exists-error'
+import { makeRegisterUserUseCase } from '@/use-cases/users/factories/make-register-user-usecase'
+
+export async function registerUserController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+): Promise<void> {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    const registerUserUseCase = makeRegisterUserUseCase()
+    await registerUserUseCase.execute({ name, email, password })
+
+    return reply.status(201).send()
+  } catch (err) {
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message })
+    }
+
+    throw err
+  }
+}
