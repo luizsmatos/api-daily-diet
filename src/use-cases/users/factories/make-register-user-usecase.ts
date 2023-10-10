22e517030635f9b24317d631168ec422f4e7eabf
Luiz Gustavo <gusstavopdm@gmail.com>
2023-10-10 12:17:25 -0300

    🎉 feat(make-register-user-usecase.ts): add factory function to create RegisterUserUseCase instance
    This commit adds a new file `make-register-user-usecase.ts` which contains a factory function `makeRegisterUserUseCase` that creates an instance of `RegisterUserUseCase` with a `PrismaUsersRepository` dependency. This factory function will be used to create instances of `RegisterUserUseCase` throughout the application.

---
+++ src/use-cases/users/factories/make-register-user-usecase.ts
@@ -0,0 +1,10 @@
+import { PrismaUsersRepository } from '@/repositories/users/prisma/prisma-users-repository'
+
+import { RegisterUserUseCase } from '../register-user-usecase'
+
+export function makeRegisterUserUseCase() {
+  const usersRepository = new PrismaUsersRepository()
+  const registerUserUseCase = new RegisterUserUseCase(usersRepository)
+
+  return registerUserUseCase
+}
