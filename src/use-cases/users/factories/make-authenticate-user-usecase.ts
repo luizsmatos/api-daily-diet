8e5062763f0c726461582f6ff440ab250817c2c1
Luiz Gustavo <gusstavopdm@gmail.com>
2023-10-10 12:53:08 -0300

    ✨ feat(make-authenticate-user-usecase.ts): add factory function to create an instance of AuthenticateUserUseCase with PrismaUsersRepository

---
+++ src/use-cases/users/factories/make-authenticate-user-usecase.ts
@@ -0,0 +1,10 @@
+import { PrismaUsersRepository } from '@/repositories/users/prisma/prisma-users-repository'
+
+import { AuthenticateUserUseCase } from '../authenticate-user-usecase'
+
+export function makeAuthenticateUserUseCase() {
+  const usersRepository = new PrismaUsersRepository()
+  const authenticateUserUseCase = new AuthenticateUserUseCase(usersRepository)
+
+  return authenticateUserUseCase
+}
