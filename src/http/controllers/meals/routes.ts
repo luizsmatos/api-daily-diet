0df6d18acfcb17233d1c6ff8f57337e2e8ec9958
Luiz Gustavo <gusstavopdm@gmail.com>
2023-10-11 09:57:30 -0300

    ✨ feat(routes.ts): add DELETE /meals/:mealId route to enable deleting a meal

--- src/http/controllers/meals/routes.ts
+++ src/http/controllers/meals/routes.ts
@@ -3,10 +3,12 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 import { createMealController } from './create-meal-controller'
 import { updateMealController } from './update-meal-controller'
+import { deleteMealController } from './delete-meal-controller'
 
 export async function mealsRoutes(app: FastifyInstance): Promise<void> {
   app.addHook('onRequest', verifyJWT)
 
   app.post('/meals', createMealController)
   app.patch('/meals/:mealId', updateMealController)
+  app.delete('/meals/:mealId', deleteMealController)
 }
