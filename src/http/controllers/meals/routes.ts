536caf3f39f8f6cd0108829b84e238964028a154
Luiz Gustavo <gusstavopdm@gmail.com>
2023-10-11 11:11:27 -0300

    🚀 feat(routes.ts): add new route '/meals/summary' to get user metrics for meals

--- src/http/controllers/meals/routes.ts
+++ src/http/controllers/meals/routes.ts
@@ -6,12 +6,14 @@ import { updateMealController } from './update-meal-controller'
 import { deleteMealController } from './delete-meal-controller'
 import { getMealController } from './get-meal-controller'
 import { listMealsController } from './list-meals-controller'
+import { getUserMetricsController } from './get-user-metrics-controller'
 
 export async function mealsRoutes(app: FastifyInstance): Promise<void> {
   app.addHook('onRequest', verifyJWT)
 
   app.post('/meals', createMealController)
   app.get('/meals', listMealsController)
+  app.get('/meals/summary', getUserMetricsController)
   app.patch('/meals/:mealId', updateMealController)
   app.delete('/meals/:mealId', deleteMealController)
   app.get('/meals/:mealId', getMealController)
