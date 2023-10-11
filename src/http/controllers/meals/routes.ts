5e995820d596d476c88e71e8d79628255c370390
Luiz Gustavo <gusstavopdm@gmail.com>
2023-10-11 10:24:42 -0300

    🚀 feat(routes.ts): add GET /meals route to list all meals

--- src/http/controllers/meals/routes.ts
+++ src/http/controllers/meals/routes.ts
@@ -5,11 +5,13 @@ import { createMealController } from './create-meal-controller'
 import { updateMealController } from './update-meal-controller'
 import { deleteMealController } from './delete-meal-controller'
 import { getMealController } from './get-meal-controller'
+import { listMealsController } from './list-meals-controller'
 
 export async function mealsRoutes(app: FastifyInstance): Promise<void> {
   app.addHook('onRequest', verifyJWT)
 
   app.post('/meals', createMealController)
+  app.get('/meals', listMealsController)
   app.patch('/meals/:mealId', updateMealController)
   app.delete('/meals/:mealId', deleteMealController)
   app.get('/meals/:mealId', getMealController)
