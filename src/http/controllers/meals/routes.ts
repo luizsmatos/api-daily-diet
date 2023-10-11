d91436884349be3f23f8294db87986df3f84e4b3
Luiz Gustavo <gusstavopdm@gmail.com>
2023-10-11 09:51:56 -0300

    ✨ feat(routes.ts): add support for updating meals by adding PATCH /meals/:mealId route

--- src/http/controllers/meals/routes.ts
+++ src/http/controllers/meals/routes.ts
@@ -2,7 +2,11 @@ import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 import { createMealController } from './create-meal-controller'
+import { updateMealController } from './update-meal-controller'
 
 export async function mealsRoutes(app: FastifyInstance): Promise<void> {
-  app.post('/meals', { onRequest: [verifyJWT] }, createMealController)
+  app.addHook('onRequest', verifyJWT)
+
+  app.post('/meals', createMealController)
+  app.patch('/meals/:mealId', updateMealController)
 }
